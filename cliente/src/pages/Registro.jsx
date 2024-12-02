92ab6c3fca19bc04c5281b5f5fc570a7de172700
Arthur Mesquita Redigolo <arthur.redigolo@estudante.ifms.edu.br>
2024-12-02 14:20:39 -0400

    tem que terminar

--- cliente/src/pages/Registro.jsx
+++ cliente/src/pages/Registro.jsx
@@ -1,9 +1,21 @@
-import { useState } from "react";
-
+import { useEffect, useState } from "react";
 export default function Registrar() {
-
-
+  const [usuarios, setUsuario]= useState([]);
+  const [gmail, setGmail]= useState([]);
+  useEffect(()=>{
+    const buscarUsuario=async()=>{
+      try{
+        const resposta= await fetch("http://localhost:3000/usuarios");
+        const dados= await resposta.json();
+        setUsuario(dados);
+        setGmail(dados);
+      }catch{
+        alert('Erro!');
+      }
+    }
+    buscarUsuario();
+  },[])
   return (
-        
+       
   );
 }
